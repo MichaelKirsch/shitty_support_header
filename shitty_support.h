303ce7e0e7450f4c7976b3e7cd7601650d822c96
Michael <michael_kirsch@t-online.de>
2021-03-18 19:38:47 +0100

    add short message to logger

--- shitty_support.h
+++ shitty_support.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <chrono>
 #include <thread>
 #include <iostream>
@@ -7,36 +8,36 @@
 #include <map>
 #include <queue>
 
-namespace ShittySupportLib{
+namespace ShittySupportLib {
     //simple clock
 
-    class SimpleClock{
+    class SimpleClock {
     public:
-        SimpleClock(){
+        SimpleClock() {
             time_point = std::chrono::steady_clock::now();
             start_time = time_point;
         }
 
-        std::chrono::milliseconds get_duration(){
-            return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now()-start_time);
+        std::chrono::milliseconds get_duration() {
+            return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start_time);
         }
 
-        long long get_duration_long(){
+        long long get_duration_long() {
             return get_duration().count();
         }
 
-        std::chrono::milliseconds tick(){
+        std::chrono::milliseconds tick() {
             auto old = time_point;
             time_point = std::chrono::steady_clock::now();
-            return std::chrono::duration_cast<std::chrono::milliseconds>(time_point-old);
+            return std::chrono::duration_cast<std::chrono::milliseconds>(time_point - old);
         }
 
-        int tick_int(){
+        int tick_int() {
             return tick().count();
         }
 
 
-        void reset_timepoint_and_restart(){
+        void reset_timepoint_and_restart() {
             time_point = std::chrono::steady_clock::now();
             start_time = time_point;
         }
@@ -47,74 +48,85 @@ namespace ShittySupportLib{
     };
 
     //bitshift stuff
-    namespace bitshifter{
-        template <class T>
-        void setbit (T& data, int whichbit) {
+    namespace bitshifter {
+        template<class T>
+        void setbit(T &data, int whichbit) {
             data |= 1UL << whichbit;
         }
 
-        template <class T>
-        void clearbit (T& data, int whichbit) {
+        template<class T>
+        void clearbit(T &data, int whichbit) {
             data &= ~(1UL << whichbit);
         }
     }
 
 
     //Debugger and logger
-    class Debugger
-    {
+    class Debugger {
     public:
-        static Debugger& instance()
-        {
+        enum Short{
+            SUCCESS,FAIL
+        };
+        static Debugger &instance() {
             static Debugger _instance;
             return _instance;
         }
+
         ~Debugger() {}
-        void log(std::string name, std::string message){
-            m_log.emplace_back(name,message);
+
+        void log(std::string name, std::string message) {
+            m_log.emplace_back(name, message);
         };
 
-        void set_max_log_size(int new_size){
-            if(new_size<m_max_log_size)
-            {
+        void log(std::string name, Short shrt) {
+            if(SUCCESS)
+                m_log.emplace_back(name, "Success");
+            else
+                m_log.emplace_back(name, "Fail");
+        }
+
+        };
+
+
+        void set_max_log_size(int new_size) {
+            if (new_size < m_max_log_size) {
                 m_log.pop_front();
             }
             m_max_log_size = new_size;
         }
 
-        void debug(std::string name,std::string data){
-            if(m_debugged_values.find(name)==m_debugged_values.end()){
-                m_debugged_values.insert(std::make_pair(name,data));
-            } else
-            {
+        void debug(std::string name, std::string data) {
+            if (m_debugged_values.find(name) == m_debugged_values.end()) {
+                m_debugged_values.insert(std::make_pair(name, data));
+            } else {
                 m_debugged_values.at(name) = std::move(data);
             }
         }
 
-        void clear_debug(){
+        void clear_debug() {
             m_debugged_values.clear();
         }
 
-        void clear_log(){
+        void clear_log() {
             while (!m_log.empty())
                 m_log.pop_front();
         }
 
-        void print_log(){
-            int counter =0;
-            std::cout << "**********"<< m_log.size() <<" Logged Values***********" <<std::endl;
-            for(auto& x:m_log){
-                std::cout << counter << "|" << x.first<<"|" << x.second<<"\n";
+        void print_log() {
+            int counter = 0;
+            std::cout << "**********" << m_log.size() << " Logged Values***********" << std::endl;
+            for (auto &x:m_log) {
+                std::cout << counter << "|" << x.first << "|" << x.second << "\n";
                 counter++;
             }
             std::cout << std::endl;
         }
 
-        void print_debug(){
-            int counter =0;
-            std::cout << "**********"<< m_debugged_values.size() <<" Debugged Values***********" <<std::endl;
-            for(auto& x:m_debugged_values){
-                std::cout << counter  << "|" << x.first<<"|" << x.second<<"\n";
+        void print_debug() {
+            int counter = 0;
+            std::cout << "**********" << m_debugged_values.size() << " Debugged Values***********" << std::endl;
+            for (auto &x:m_debugged_values) {
+                std::cout << counter << "|" << x.first << "|" << x.second << "\n";
                 counter++;
             }
             std::cout << std::endl;
@@ -124,10 +136,21 @@ namespace ShittySupportLib{
         Debugger() {
 
         }
-        Debugger(const Debugger& );
-        Debugger & operator = (const Debugger &);
-        int m_max_log_size=50;
-        std::deque<std::pair<std::string,std::string>> m_log;
-        std::map<std::string,std::string> m_debugged_values;
+
+        Debugger(const Debugger &);
+
+        Debugger &operator=(const Debugger &);
+
+        int m_max_log_size = 50;
+        std::deque<std::pair<std::string, std::string>> m_log;
+        std::map<std::string, std::string> m_debugged_values;
     };
+
+    namespace TaskManager {
+
+
+
 }
+
+
+
