40d8b0ae7135c1a3e78775e0bc11fad911b19d76
Michael <michael_kirsch@t-online.de>
2021-03-19 00:02:23 +0100

    fix stupid mistake in logger

--- shitty_support.h
+++ shitty_support.h
@@ -79,7 +79,7 @@ namespace ShittySupportLib {
         };
 
         void log(std::string name, Short shrt) {
-            if(SUCCESS)
+            if(shrt)
                 m_log.emplace_back(name, "Success");
             else
                 m_log.emplace_back(name, "Fail");
